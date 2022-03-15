7c0f9da178e5060b60a67b72be008d4347cfa289
Felix Mochoge <makbakari@gmail.com>
2022-03-15 21:07:18 +0300

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,25 +18,30 @@ void print_times_table(int n)
 				d = c / 100;
 				e = (c /  10) % 10;
 				f = (c % 100) % 10;
-				if (b == 0){
+				if (b == 0)
+				{
 					_putchar('0');
 				}
-				else if (c < 10){
+				else if (c < 10)
+				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + f);
 				}
-				else if (c < 100){
+				else if (c < 100)
+				{
 					_putchar(' ');
 					_putchar('0' + e);
 					_putchar('0' + f);
 				}
-				else{
+				else
+				{
 					_putchar('0' + d);
 					_putchar('0' + e);
 					_putchar('0' + f);
 				}
-				if (b < n){
+				if (b < n)
+				{
 					_putchar(',');
 					_putchar(' ');
 				}
