a7a2cb8b0474d4ab8da648aca6bcdcadfdd74103
Felix Mochoge <makbakari@gmail.com>
2022-03-21 20:28:46 +0300

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,16 @@
  */
 void puts2(char *str)
 {
-	  for (i = 0; *str; str += 2) {
-    _putchar(*str);
-  }
+	int i;
+	int j;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		;
+	}
+	for (j = 0 ; j < i ; j = j + 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
 }
