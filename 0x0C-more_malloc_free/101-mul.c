179ceeba11c4d14a37b540f6fb0b6a34cfaca170
Felix Mochoge <makbakari@gmail.com>
2022-04-07 22:35:51 +0300

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,39 +1,68 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- *@argc: number of arguments
- *@argv: string with arguments
- * Return: Always 0 (Success)
- */
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
+int _isdigit(char *argv)
+{
+	int i;
 
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+/**
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return (result);
+}
+/**
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
 int main(int argc, char *argv[])
 {
-	long int multi;
-	int i, j;
+	int i;
 
-	multi = 0;
+	malloc();
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (_isdigit(argv[i]))
 		{
-			if (!(isdigit(argv[i][j])))
-			{
-				printf("Error\n");
-				exit(98);
-			}
+			printf("Error\n");
+			exit(98);
 		}
 	}
-	multi = atoi(argv[1]) * atoi(argv[2]);
-	printf("%ld\n", multi);
 	return (0);
 }
